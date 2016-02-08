2369defc459158e046eceb7ef13b00e5d85b8d31
Anton Matveev <aush.tx@gmail.com>
2016-02-08 13:24:32 +0700

    fix umd library name

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'index.js',
-    library: 'ReactRectangle',
+    library: 'ReactStack',
     libraryTarget: 'umd',
   },
   externals: {
